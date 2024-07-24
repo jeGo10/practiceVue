b59278c2b972cdf94ab848eb895917910deba67c
jeGo10 <jericogo7001@gmail.com>
2024-07-24 11:45:51 +0800

    Fixed css style for sidebar

--- resources/js/Components/NavLink.vue
+++ resources/js/Components/NavLink.vue
@@ -14,8 +14,8 @@ const props = defineProps({
 
 const classes = computed(() =>
     props.active
-        ? 'flex items-center px-1 py-3 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-slate-400 hover:text-slate-50 focus:outline-none focus:border-indigo-700 no-underline'
-        : 'flex items-center px-1 py-3 border-b-2 border-transparent text-sm font-medium leading-5 text-slate-400 hover:text-slate-50 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 no-underline'
+        ? 'flex items-center py-3 text-lg font-medium leading-5 text-gray-300 hover:text-white bg-slate-600 rounded-md focus:outline-none no-underline'
+        : 'flex items-center py-3 text-lg font-medium leading-5 text-gray-300 hover:text-white hover:bg-slate-600 rounded-md focus:outline-none focus:text-gray-400 no-underline'
 );
 </script>
 
